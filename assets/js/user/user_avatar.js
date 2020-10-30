17f9620746a47cb09cf83e1a2fde91f6e37f653c
cheng <89339841@qq.com>
2020-10-30 10:36:00 +0800

    个人中心功能完成

--- assets/js/user/user_avatar.js
+++ assets/js/user/user_avatar.js
@@ -11,4 +11,56 @@ $(function () {
 
   // 1.3 创建裁剪区域
   $image.cropper(options)
+
+  //选择文件功能
+  $('#chooseImg').on('click',function () {
+    $('#file').trigger('click')
+  })
+
+  //修改裁剪图片
+  var layer = layui.layer
+  $('#file').on('change',function (e) {
+    // 拿到用户选择的文件  这里e.target可以换成this
+    var file = e.target.files[0]
+    //非空校验
+    if(file === undefined) {
+      return layer.msg('请选择上传的图片')
+    }
+    //根据用户选择的文件生成 url值
+    var newImgUrl = URL.createObjectURL(file)
+    //使用cropper插件
+    $image
+   .cropper('destroy')      // 销毁旧的裁剪区域
+   .attr('src', newImgUrl)  // 重新设置图片路径
+   .cropper(options)        // 重新初始化裁剪区域
+  })
+
+  //提交图像并更新
+  $('#btnUpload').on('click',function () {
+    //获取base64 类型的头像(字符串)
+    var dataURL = $image
+      .cropper('getCroppedCanvas', { // 创建一个 Canvas 画布
+        width: 100,
+        height: 100
+      })
+      .toDataURL('image/png')       // 将 Canvas 画布上的内容，转化为 base64 格式的字符串
+      // console.log(dataURL);
+
+      //发送ajax请求
+      $.ajax({
+        url:'/my/update/avatar',
+        data:{
+          avatar:dataURL
+        },
+        type:'post',
+        success: function (res) {
+          if(res.status !== 0) {
+            return layer.msg(res.message)
+          }
+          layer.msg('跟换头像成功')
+          //更新父框架中的图像
+          window.parent.getUserinfo()
+        }
+      })
+  })
 })
